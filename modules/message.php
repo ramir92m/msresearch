c7c1b80141005c4f2acf421c35a6f6e515b37f6f
morbidtritz <manarpaacramir@gmail.com>
2012-12-28 00:15:54 +0800

    Separate for users

--- modules/message.php
+++ modules/message.php
@@ -4,18 +4,59 @@ require '../class/Database.php';
 //
 $db = new Database();
 
-$dept = $_SESSION['course'];
-
-
-
-$mysql = $db->selectData("*", "bulletin", "dept = '$dept'");
-
-while($row = mysql_fetch_assoc($mysql))
+if(isset($_GET['type']))
 {
-    echo "<div class='well media' style='margin:0 10px;'><div class='media-body'><h5 class='media-header'>".$row['sender']."</h5><p>".$row['msg']."</p> <i style='font-size:8pt;'>Post on Jun 12 2012</i></div></div><br/>";
+    $type = $_GET['type'];
+    
+    if($type == "announce")
+    {
+            $occupation = $_SESSION['occupation'];
+
+            if($occupation == "Student")
+            {
+                $dept = $_SESSION['course'];
+
+
+
+                $mysql = $db->selectData("*", "bulletin", "dept = '$dept'");
+
+                while($row = mysql_fetch_assoc($mysql))
+                {
+                    echo "<div class='well media' style='margin:0 10px;'><div class='media-body'><h5 class='media-header'>".$row['sender']."</h5><p>".$row['msg']."</p> <i style='font-size:8pt;'>Post on Jun 12 2012</i></div></div><br/>";
+                }
+            }
+
+            if($occupation == "Moderator")
+            {
+                $dept = $_SESSION['coursehandle'];
+
+
+
+                $mysql = $db->selectData("*", "bulletin", "dept = '$dept'");
+
+                while($row = mysql_fetch_assoc($mysql))
+                {
+                    echo "<div class='well media' style='margin:0 10px;'><div class='media-body'><h5 class='media-header'>".$row['sender']."</h5><p>".$row['msg']."</p> <i style='font-size:8pt;'>Post on Jun 12 2012</i></div></div><br/>";
+                }
+            }
+            if($occupation == "Moderator")
+            {
+                $dept = $_SESSION['coursehandle'];
+
+
+
+                $mysql = $db->selectData("*", "bulletin");
+
+                while($row = mysql_fetch_assoc($mysql))
+                {
+                    echo "<div class='well media' style='margin:0 10px;'><div class='media-body'><h5 class='media-header'>".$row['sender']."</h5><p>".$row['msg']."</p> <i style='font-size:8pt;'>Post on Jun 12 2012</i></div></div><br/>";
+                }
+            }
+    }
+    
+    if($type == "message")
+    {
+        
+    }
 }
-
-
-
-
 ?>
