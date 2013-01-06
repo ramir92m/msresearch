b531e247c48d374a2c82d53f11ef0baad5f02e31
morbidtritz <manarpaacramir@gmail.com>
2013-01-07 02:34:01 +0800

    inbox functionality

--- modules/inbox.php
+++ modules/inbox.php
@@ -3,62 +3,81 @@
 <?php
 session_start();
 include("../class/Database.php");
-
+$acctID = $_SESSION['acct_ID'];
+$user = array();
+$msg = array();
 $db = new Database();
+$i = 0;
+$query = $db->selectData("sender_ID,msg,sname", "message","receiver_id = $acctID ");
+//while($row = mysql_fetch_array($query))
+//{
+//    $msg[] = $row['sname'];
+//    $user[$row['sname']] = $row['sender_ID'];
+//}
 
-
-echo '<div class="tabbable">  
-                                            <ul class="nav nav-tabs">
-                                                <li class="active"><a href="#tab1" data-toggle="tab">Message</a></li>
-                                                <li><a href="#tab2" data-toggle="tab">Compose New Message</a></li>
-                                            </ul>
-                                            <div class="tab-content">
-                                                
-                                                <div class="tab-pane active" id="tab1">
-                                                    <div class="msg-header">
-                                                        <div class="span2">
-                                                            <b>From</b>
-                                                        </div>
-                                                    <div class="span9">
-                                                        <b>Messages</b>
-                                                    </div>       
-                                                </div>';
-                                                
+//                                                $c = array_unique($user);                                                 
+//                                                //$count = array_count_values($user);
+//                                                print_r($c);
                                                     
+
+//                                            echo '<div class="tabbable">  
+//                                            <ul class="nav nav-tabs">
+//                                                <li class="active"><a href="#tab1" data-toggle="tab">Message</a></li>
+//                                                <li><a href="#tab2" data-toggle="tab">Compose New Message</a></li>
+//                                            </ul>
+//                                            <div class="tab-content">
+//                                                
+//                                                <div class="tab-pane active" id="tab1">
+//                                                    <div class="msg-header">
+//                                                        <div class="span2">
+//                                                            <b>From</b>
+//                                                        </div>
+//                                                    <div class="span9">
+//                                                        <b>Messages</b>
+//                                                    </div>       
+//                                                </div>';
+                                                 
+                                                while($row = mysql_fetch_array($query))
+                                                {
+                                                    $user[$row['sname']] = $row['msg'];
+                                                    
+                                                }
+                                                
+                                                foreach($user as $column =>$value)
+                                                {
+                                                    
+                                                
                                                 
                                                     echo' <a href="#">
                                                         <div class="msg-content">
                                                             <div class="span2" style="display: box;">
-                                                                <span>Shit</span>
+                                                                <span>'.$column.'</span>
                                                             </div>
                                                             <div class="span9">
-                                                                <b><p>5 Message</p></b>
+                                                                <b><p>'.$value.'</p></b>
                                                             </div>
                                                         </div>
                                                     </a>';
-                                                    $i++;
+                                                  }  
+                                                   
+                                                    
+                                                
+                                                
+//                                                $c = array_unique($user); 
+//                                                $count = array_count_values($user);
+//                                                 foreach($c as $value)
+//                                                 {
+//                                                     echo "From ".$value." Message ".$count[$value]." " ;
+//                                                 }
+ 
+                                                    
+                                                
+                                                    
+                                                    
                                                
                                   
                                   
                                   
-                                                echo '
-                                                </div>
-                                                <div class="tab-pane" id="tab2">
-                                                    <fieldset>
-                                                    <legend>Inbox</legend>
-
-                                                    <div id="msgbody" class="span12" placeholder="Employee ID"></div>
-                                                    <hr/>
-                                                    <input type="text" class="span5" /><br/>
-                                                    <textarea placeholder="Search Message" class="span5" style="resize: none;"></textarea><br/>
-                                                    <button type="button" class="btn btn-primary">Send</button>
-                                                    <select>
-                                                        <option>Announcement</option>
-                                                        <option>Message</option>
-                                                    </select>
-                                                </fieldset>
-                                            </div>
-                                        </div>
-                                </div>';
+                                                
 
 ?>
